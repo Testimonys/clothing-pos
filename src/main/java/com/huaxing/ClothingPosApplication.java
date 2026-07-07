78e0a111f3d770936d1e31e5c3d2b6d7f73e76bf
Testimonys <13761157903@163.com>
2026-07-07 14:42:33 +0800

    feat: initialize Spring Boot project with dependencies

---
+++ src/main/java/com/huaxing/ClothingPosApplication.java
@@ -0,0 +1,11 @@
+package com.huaxing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingPosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClothingPosApplication.class, args);
+    }
+}
